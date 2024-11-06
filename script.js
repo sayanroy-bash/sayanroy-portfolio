313d6358a0a301c6cc3547e0be283f1070d8ac9e
sayanroy-bash <sayanroy975@gmail.com>
2024-11-06 18:20:11 +0530

    resume button bug fixed

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function downloadResume() {
-    window.open("./SayanRoy-FrontendDeveloper(React)-NUhj.pdf", '_blank');
-  }
+  const link = document.createElement('a');
+  link.href = "./SayanRoy-FrontendDeveloper(React)-NUhj.pdf";
+  link.download = "SayanRoy-FrontendDeveloper(React)-NUhj.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
   
